9bc2f146b4e5e12b49fcf6c86dd0a8f25811f8ec
naveedalirehmani <naveedalirehmani321@gmail.com>
2024-02-21 13:50:31 +0500

    hello worlds

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, worlds!');
+  res.send('Hello, worldss!');
 });
 
 app.listen(port, () => {
