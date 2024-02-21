71e03c7a8bb5717061ca16f962a7e21498065400
naveedalirehmani <naveedalirehmani321@gmail.com>
2024-02-21 14:22:44 +0500

    deploy pipleline flowerbox

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,10 @@
 import express, { Request, Response } from 'express';
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, world!');
+  res.send('Hello, worlds!');
 });
 
 app.listen(port, () => {
