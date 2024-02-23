f2b238f11b6e53f738a63ea0630858a83a402dd4
naveedalirehmani <naveedalirehmani321@gmail.com>
2024-02-23 21:34:25 +0500

    removed extra comments

--- server/src/routes/authentication/authentication.router.ts
+++ server/src/routes/authentication/authentication.router.ts
@@ -7,12 +7,8 @@ import {
   logoutHandler,
 } from "../../controller/authentication/authentication.controller";
 
-// signup-handler
 awsRouter.post("/sign-up", signupHandler);
-
-//login handler-handler
 awsRouter.post("/login", loginHandler);
-
 awsRouter.post("/logout", logoutHandler);
 
 export default awsRouter;
