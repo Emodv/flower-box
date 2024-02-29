75e141b7ef725821974c0b0196ca77b34d739b1a
naveedalirehmani <naveedalirehmani321@gmail.com>
2024-02-29 17:55:23 +0500

    added stripe pricing enums

---
+++ server/src/enums/stripeEnums.ts
@@ -0,0 +1,7 @@
+enum SubscriptionEnum {
+  "1 month subscription" = 2999, // $29.99,
+  "3 months subscription" = 6000, // $20 per month, 3 months = $60,
+  "6 months subscription" = 8999, // $14.99 per month, 6 months = $89.94
+}
+
+export { SubscriptionEnum };
