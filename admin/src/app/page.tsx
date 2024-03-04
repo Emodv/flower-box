c932e5dc81fb2a4a036b1afa52ed70975e15dfd9
naveedalirehmani <naveedalirehmani321@gmail.com>
2024-03-04 20:10:55 +0500

    updated workflow to include types

--- admin/src/app/page.tsx
+++ admin/src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
       <div className="container">
-        <p className="text-primary text-4xl">hello admins</p>
+        <p className="text-primary text-4xl">hello admin1</p>
       </div>
     </main>
   );
--- portal/src/app/page.tsx
+++ portal/src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
       <div className="container">
-        <p className="text-primary text-4xl">hello world</p>
+        <p className="text-primary text-4xl">hello world1</p>
       </div>
     </main>
   );
