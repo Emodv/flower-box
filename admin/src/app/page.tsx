2406797fd4d2ca81d0a41fecedba9fb49b0afcfd
naveedalirehmani <naveedalirehmani321@gmail.com>
2024-03-05 11:51:27 +0500

    typescript build error potential fix.

--- admin/src/app/page.tsx
+++ admin/src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
       <div className="container">
-        <p className="text-primary text-4xl">hello admin3</p>
+        <p className="text-primary text-4xl">hello admin4</p>
       </div>
     </main>
   );
--- portal/src/app/page.tsx
+++ portal/src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
       <div className="container">
-        <p className="text-primary text-4xl">hello world2</p>
+        <p className="text-primary text-4xl">hello world4</p>
       </div>
     </main>
   );
