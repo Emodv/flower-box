ba84cdd6298ae63e63d66da173efbc9a2618f61d
naveedalirehmani <naveedalirehmani321@gmail.com>
2024-03-05 12:21:19 +0500

    server workflow namespace process erorr fixed.

--- admin/src/app/page.tsx
+++ admin/src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
       <div className="container">
-        <p className="text-primary text-4xl">hello admin5</p>
+        <p className="text-primary text-4xl">hello admin7</p>
       </div>
     </main>
   );
