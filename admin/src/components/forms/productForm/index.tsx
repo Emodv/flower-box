78a4c2f0c46769f1dda8e139c178ac94f8d186f0
naveedalirehmani <naveedalirehmani321@gmail.com>
2024-03-08 12:02:49 +0500

    export zod schema to use client

--- admin/src/components/forms/productForm/index.tsx
+++ admin/src/components/forms/productForm/index.tsx
@@ -18,7 +18,36 @@ import { useCallback, useState } from "react";
 import { useToast } from "@/components/ui/use-toast";
 import Image from "next/image";
 import { MinusCircle, Trash } from "lucide-react";
-import { addProductSchema } from "@/schema/zod";
+// import { addProductSchema } from "@/schema/zod";
+
+// keeping this schema interface in 'use client' because of the web api File type"
+const addProductSchema = z.object({
+  productName: z
+    .string({ required_error: "Product Name is required." })
+    .min(2, {
+      message: "Product Name must contain atleast 2 charactters",
+    }),
+  productId: z
+    .string({ required_error: "Product ID Name is required." })
+    .min(2, {
+      message: "Product ID must contain atleast 2 charactters",
+    }),
+  description: z
+    .string({
+      required_error: "Description is required.",
+    })
+    .min(30, { message: "Description must be at least 30 characters." }),
+  price: z.string({
+    required_error: "Price is required.",
+  }),
+  productImages: z
+    .array(z.instanceof(File), {
+      required_error: "Please upload at least one product image.",
+    })
+    .nonempty({
+      message: "Please upload at least one product image.",
+    }),
+});
 
 type FormSchemaType = z.infer<typeof addProductSchema>;
 
