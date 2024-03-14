a713751c91bf5ede107d5e38a5a207a98c009a8c
naveedalirehmani <naveedalirehmani321@gmail.com>
2024-03-14 20:14:48 +0500

    added products table.

--- admin/src/app/dashboard/all-products/page.tsx
+++ admin/src/app/dashboard/all-products/page.tsx
@@ -1,28 +1,28 @@
 "use client";
 
+import { DataTable } from "@/components/DataTable";
+import React from "react";
+import { columns } from "./columns";
 import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { fetchPaginatedProducts } from "@/services/adminService";
 import PageTitle from "@/components/PageTitle";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { ProductTypes } from "@/types/types";
 
-type Product = {
-  id: number;
-  name: string;
-};
+type Props = {};
 
-const ProductsPage: React.FC = () => {
+interface Product {
+  data: {
+    data : ProductTypes.Product;
+  }
+  // user: user;
+}
+
+function ProductList({}: Props) {
   const [page, setPage] = useState(1);
   const pageSize = 10;
 
-  const { data, isLoading, isError, error } = useQuery({
+  const { data, isLoading, isError, error } = useQuery<Product>({
     queryKey: ["products", page, pageSize],
     queryFn: () => fetchPaginatedProducts({ page, pageSize }),
     refetchOnWindowFocus: false,
@@ -35,28 +35,68 @@ const ProductsPage: React.FC = () => {
         Error: {error instanceof Error ? error.message : "An error occurred"}
       </div>
     );
-  console.log(data, "data");
+  
+  console.log(data?.data.data, "data");
+
   return (
     <div className="flex w-full flex-col gap-5">
       <PageTitle title="Product list" />
-      <div className="container">
-        <ul>
-          {data?.data?.data.map((product: Product) => (
-            <li key={product.id}>{product.name}</li>
-          ))}
-        </ul>
-        <div>
-          <button
-            onClick={() => setPage((old) => Math.max(old - 1, 1))}
-            disabled={page === 1}
-          >
-            Previous Page
-          </button>
-          <button onClick={() => setPage((old) => old + 1)}>Next Page</button>
-        </div>
-      </div>
+      <DataTable columns={columns} data={data?.data?.data} />
     </div>
   );
-};
+}
 
-export default ProductsPage;
+export default ProductList;
+
+// import { useState } from "react";
+// import { useQuery } from "@tanstack/react-query";
+// import { fetchPaginatedProducts } from "@/services/adminService";
+// import PageTitle from "@/components/PageTitle";
+
+// type Product = {
+//   id: number;
+//   name: string;
+// };
+
+// const ProductsPage: React.FC = () => {
+//   const [page, setPage] = useState(1);
+//   const pageSize = 10;
+
+//   const { data, isLoading, isError, error } = useQuery({
+//     queryKey: ["products", page, pageSize],
+//     queryFn: () => fetchPaginatedProducts({ page, pageSize }),
+//     refetchOnWindowFocus: false,
+//   });
+
+//   if (isLoading) return <div>Loading...</div>;
+//   if (isError)
+//     return (
+//       <div>
+//         Error: {error instanceof Error ? error.message : "An error occurred"}
+//       </div>
+//     );
+//   console.log(data, "data");
+//   return (
+//     <div className="flex w-full flex-col gap-5">
+//       <PageTitle title="Product list" />
+//       <div className="container">
+//         <ul>
+//           {data?.data?.data.map((product: Product) => (
+//             <li key={product.id}>{product.name}</li>
+//           ))}
+//         </ul>
+//         <div>
+//           <button
+//             onClick={() => setPage((old) => Math.max(old - 1, 1))}
+//             disabled={page === 1}
+//           >
+//             Previous Page
+//           </button>
+//           <button onClick={() => setPage((old) => old + 1)}>Next Page</button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ProductsPage;
