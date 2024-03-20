d15a8344dbae370d2a474fd0550146b1eba64eb2
naveedalirehmani <naveedalirehmani321@gmail.com>
2024-03-20 18:54:50 +0500

    components and next.js configrations.

---
+++ portal/next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["flowerbox.s3.ca-central-1.amazonaws.com"],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/dashboard",
+        permanent: true,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
