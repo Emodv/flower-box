1d6e762acf71986fa7e37f9be0aa702191cd8aa8
naveedalirehmani <naveedalirehmani321@gmail.com>
2024-03-21 21:29:47 +0500

    removed redirect.

--- portal/next.config.js
+++ portal/next.config.js
@@ -3,15 +3,15 @@ const nextConfig = {
   images: {
     domains: ["flowerbox.s3.ca-central-1.amazonaws.com"],
   },
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/dashboard",
-        permanent: true,
-      },
-    ];
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/",
+  //       destination: "/dashboard",
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
