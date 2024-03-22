14370ed6f9811aadd7e44379818b8075a84cb58a
naveedalirehmani <naveedalirehmani321@gmail.com>
2024-03-22 22:59:32 +0500

    layout changes for fonts.

--- portal/src/app/layout.tsx
+++ portal/src/app/layout.tsx
@@ -1,13 +1,15 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { cn } from "../lib/utils";
-import { Toaster } from "@/components/ui/toaster";
+import type { Metadata } from "next";
+import { inter} from "@/font/font";
+
 import { ThemeProvider } from "@/lib/theme-provider";
 import QueryProvider from "@/lib/react-query-provider";
-import Navbar from "@/components/navbar/navbar";
 
-const inter = Inter({ subsets: ["latin"] });
+import { Toaster } from "@/components/ui/toaster";
+import Navbar from "@/components/navbar/navbar";
 
 export const metadata: Metadata = {
   title: "Flower Box",
