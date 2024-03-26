127ee63caa1a9dde58c876f25d4def50438f93f5
naveedalirehmani <naveedalirehmani321@gmail.com>
2024-03-26 21:54:57 +0500

    added home page components

--- portal/src/app/page.tsx
+++ portal/src/app/page.tsx
@@ -1,23 +1,36 @@
 import React from "react";
 import Header from "./(home)/header";
-import ProductsByCatogories from "@/components/products";
+
+import ProductsByCatogories from "@/components/products/productsByCategories";
+import TopProducts from "@/components/products/productsWithDiscount";
+
 import { Category } from "@/types/productTypes";
+import Banner from "@/components/banners/banner";
+import JoinUs from "@/components/joinus/joinus";
+import Navbar from "@/components/navbar/navbar";
 
 type Props = {};
 
 function Page({}: Props) {
   return (
-    <div className="">
-      <Header></Header>
-      <ProductsByCatogories
-        categories={[
-          Category.winter,
-          Category.autumn,
-          Category.anniversary,
-          Category.summer,
-        ]}
-      ></ProductsByCatogories>
-    </div>
+    <>
+      <Navbar></Navbar>
+      <div className="">
+        <Header />
+        <ProductsByCatogories
+          categories={[
+            Category.romantic,
+            Category.anniversary,
+            // Category.spring,
+            // Category.anniversary,
+            // Category.summer,
+          ]}
+        />
+        <TopProducts categories={[Category.romantic]} />
+        <Banner></Banner>
+        <JoinUs></JoinUs>
+      </div>
+    </>
   );
 }
 
