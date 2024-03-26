e6b8823f44948b8655f70ce0f5cd01454e82d357
naveedalirehmani <naveedalirehmani321@gmail.com>
2024-03-26 21:55:20 +0500

    added listing by categories component.

---
+++ portal/src/components/products/productsByCategories.tsx
@@ -0,0 +1,130 @@
+"use client";
+
+import React from "react";
+import Slider from "react-slick";
+import { useQuery } from "@tanstack/react-query";
+import { getProductsByCategories } from "@/services";
+import { ProductTypes } from "@/types/types";
+import { Category } from "@/types/productTypes";
+import Link from "next/link";
+import ProductCards from "../cards/productCards";
+import Title from "../title/title";
+import { ArrowRight } from "lucide-react";
+
+interface ProductsByCategoriesProps {
+  categories: Category[];
+}
+
+interface ProductWithPlaceholder extends ProductTypes.Product {
+  placeholder?: boolean;
+}
+
+interface ProductsByCategoryI {
+  data: {
+    data: {
+      [key in Category]?: ProductWithPlaceholder[];
+    };
+  };
+}
+
+const settings = {
+  className: "center custom-slider",
+  infinite: true,
+  centerPadding: "60px",
+  slidesToShow: 4,
+  swipeToSlide: true,
+  arrows: false,
+  afterChange: function (index: number) {
+    console.log(`Slider Changed to: ${index + 1}`);
+  },
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2
+      }
+    },
+    {
+      breakpoint: 570,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+};
+
+const ProductsByCategories = ({ categories }: ProductsByCategoriesProps) => {
+  const { data, isLoading, isError, error } = useQuery<ProductsByCategoryI>({
+    queryKey: ["productsByCategories"],
+    queryFn: () => getProductsByCategories({ categories }),
+    refetchOnWindowFocus: false,
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError)
+    return (
+      <div>
+        Error: {error instanceof Error ? error.message : "Unknown error"}
+      </div>
+    );
+
+  return (
+    <div className="container py-14">
+      <Title>Products by Categories</Title>
+      {categories.map((category) => {
+        const products = data?.data?.data[category];
+
+        if (!products || products.length === 0) {
+          return null;
+        }
+
+        const extendedProducts = [...products];
+        while (extendedProducts.length < 4) {
+          extendedProducts.push({
+            id: 1,
+            name: "Comming soon...",
+            assets: ["/placeholderProduct.jpg"],
+            description: "",
+            price: 0,
+            createdAt: "",
+            categories: [""],
+            placeholder:true
+          });
+        }
+
+        return (
+          <div key={category} className="my-10">
+            <div className="flex justify-between px-6 py-4">
+              <h3 className="text-lg capitalize text-primary">{category}</h3>
+              <Link
+                href="/"
+                className="text-md flex items-center justify-center gap-2 text-subtle transition-all hover:gap-3"
+              >
+                See more <ArrowRight size={18} />
+              </Link>
+            </div>
+            <Slider {...settings}>
+              {extendedProducts.map((item) => (
+                <ProductCards key={item.id} {...item} />
+              ))}
+            </Slider>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default ProductsByCategories;
