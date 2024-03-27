c7697d118833a7c499e4195c1077077b308543db
naveedalirehmani <naveedalirehmani321@gmail.com>
2024-03-27 21:12:07 +0500

    search bar finishing.

--- portal/src/app/category/page.tsx
+++ portal/src/app/category/page.tsx
@@ -1,14 +1,27 @@
+"use client";
+
 import SearchBar from "@/components/search/seachbar";
 import React from "react";
+import ProductsByCatogories from "@/components/products/productsByCategories";
+import { Category } from "@/types/productTypes";
 
 type Props = {};
 
 function Page({}: Props) {
   return (
     <div>
-      <div className="my-20">
-        <SearchBar></SearchBar>
+      <div className="sticky top-0 z-10">
+      <SearchBar></SearchBar>
       </div>
+      <ProductsByCatogories
+          categories={[
+            Category.romantic,
+            Category.anniversary,
+            Category.spring,
+            Category.anniversary,
+            Category.summer,
+          ]}
+        />
     </div>
   );
 }
