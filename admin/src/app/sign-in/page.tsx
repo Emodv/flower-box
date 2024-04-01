2194489b47259e8e284f363e13e8001023a3ec73
naveedalirehmani <naveedalirehmani321@gmail.com>
2024-04-01 23:10:05 +0500

    updated admin authentication page.

--- admin/src/app/sign-in/page.tsx
+++ admin/src/app/sign-in/page.tsx
@@ -26,6 +26,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useMutation } from "@tanstack/react-query";
 import { AxiosError } from "axios";
 import { Loader2 } from "lucide-react";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
@@ -57,7 +58,7 @@ export default function LoginAccount() {
     },
     onError: (error: CustomAxiosError) => {
       setFormLoading(false);
-      const message = error.response?.data?.message || error.message
+      const message = error.response?.data?.message || error.message;
       toast({
         title: message,
         variant: "destructive",
@@ -69,65 +70,91 @@ export default function LoginAccount() {
   const onSubmit = async (values: FormSchemaType) => {
     setFormLoading(true);
     mutation.mutate({
-      ...values
+      ...values,
     });
   };
 
   return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="space-y-6 w-screen h-screen flex items-center justify-center"
-      >
-        <Card className="min-w-[400px]">
-          <CardHeader className="space-y-1">
-            <CardTitle className="text-center text-2xl">Sign in</CardTitle>
-            <CardDescription className="text-center">
-              Enter your email and password to login
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="grid gap-4">
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem className="flex-1">
-                  <FormLabel className="text-md font-normal text-black dark:text-white">
-                    Email
-                  </FormLabel>
-                  <FormControl>
-                    <Input className="mt-2" placeholder="email" {...field} />
-                  </FormControl>
-                  <FormMessage className="mt-2 dark:text-red-800" />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem className="flex-1">
-                  <FormLabel className="text-md font-normal text-black dark:text-white">
-                    Password
-                  </FormLabel>
-                  <FormControl>
-                    <Input className="mt-2" placeholder="password" type="password" {...field} />
-                  </FormControl>
-                  <FormMessage className="mt-2 dark:text-red-800" />
-                </FormItem>
-              )}
-            />
-          </CardContent>
-          <CardFooter className="flex flex-col">
-            <Button type="submit" disabled={isFormLoading} className="w-full text-white">
-              {isFormLoading && (
-                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-              )}
-              Submit
-            </Button>
-          </CardFooter>
-        </Card>
-      </form>
-    </Form>
+    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+      <div className="hidden bg-muted lg:block">
+        <Image
+          src="/1.jpg"
+          alt="Image"
+          width="1920"
+          height="1080"
+          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+      <div className="flex items-center justify-center py-12">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex h-screen w-screen items-center justify-center space-y-6"
+          >
+            <Card className="min-w-[400px]">
+              <CardHeader className="space-y-1">
+                <CardTitle className="text-center text-2xl">Sign in</CardTitle>
+                <CardDescription className="text-center">
+                  Enter your email and password to login
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="grid gap-4">
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
+                      <FormLabel className="text-md font-normal text-black dark:text-white">
+                        Email
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          className="mt-2"
+                          placeholder="email"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage className="mt-2 dark:text-red-800" />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
+                      <FormLabel className="text-md font-normal text-black dark:text-white">
+                        Password
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          className="mt-2"
+                          placeholder="password"
+                          type="password"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage className="mt-2 dark:text-red-800" />
+                    </FormItem>
+                  )}
+                />
+              </CardContent>
+              <CardFooter className="flex flex-col">
+                <Button
+                  type="submit"
+                  disabled={isFormLoading}
+                  className="w-full text-white"
+                >
+                  {isFormLoading && (
+                    <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                  )}
+                  Submit
+                </Button>
+              </CardFooter>
+            </Card>
+          </form>
+        </Form>
+      </div>
+    </div>
   );
 }
