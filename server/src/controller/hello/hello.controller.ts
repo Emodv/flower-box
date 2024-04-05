a2c8d69e6411c2971ed6735d47ad6528e7923388
naveedalirehmani <naveedalirehmani321@gmail.com>
2024-04-05 14:51:45 +0500

    env consoles

--- server/src/controller/hello/hello.controller.ts
+++ server/src/controller/hello/hello.controller.ts
@@ -1,7 +1,22 @@
 import { Request, Response } from "express";
 
 function sayHello(request: Request, response: Response): void {
-  response.status(200).send("hello9");
+  const DATABASE_URL = process.env.DATABASE_URL;
+  const CLIENT_SERVER_2 = process.env.CLIENT_SERVER_2;
+  const COOKIE_CLIENT = process.env.COOKIE_CLIENT;
+
+  console.log(DATABASE_URL, "DATABASE_URL");
+  console.log(CLIENT_SERVER_2, "CLIENT_SERVER_2");
+  console.log(COOKIE_CLIENT, "COOKIE_CLIENT");
+
+  response.status(200).json({
+    message: "hello10",
+    data: {
+      DATABASE_URL,
+      CLIENT_SERVER_2,
+      COOKIE_CLIENT,
+    },
+  });
 }
 
 export = { sayHello };
