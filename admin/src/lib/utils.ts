8d18b51532e42b2f8d05ac8353b6df9ca01ffe55
naveedalirehmani <naveedalirehmani321@gmail.com>
2024-04-17 23:48:27 +0500

    added helper functions

--- admin/src/lib/utils.ts
+++ admin/src/lib/utils.ts
@@ -4,3 +4,20 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatDate(dateString: string): string {
+  const date = new Date(dateString);
+  return date.toLocaleDateString('en-GB', {
+    day: '2-digit',
+    month: '2-digit',
+    year: '2-digit'
+  });
+}
+
+export function truncateString(str:string, num:number) {
+  if (str.length > num) {
+    return str.slice(0, num) + "...";
+  } else {
+    return str;
+  }
+}
