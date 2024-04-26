c32dc8814e1ccaf682b4765601ac4a23e5e81d69
naveedalirehmani <naveedalirehmani321@gmail.com>
2024-04-26 23:01:30 +0500

    workflow test

--- server/src/controller/hello/hello.controller.ts
+++ server/src/controller/hello/hello.controller.ts
@@ -10,7 +10,7 @@ function sayHello(request: Request, response: Response): void {
   console.log(COOKIE_CLIENT, "COOKIE_CLIENT");
 
   response.status(200).json({
-    message: "hello10",
+    message: "hello100",
     data: {
       DATABASE_URL,
       CLIENT_SERVER_2,
