f683c343563b52fdfb4dd01e8c266458daeeaa5f
naveedalirehmani <naveedalirehmani321@gmail.com>
2024-05-16 02:11:04 +0500

    update workflower for server

--- server/src/controller/hello/hello.controller.ts
+++ server/src/controller/hello/hello.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 
 function sayHello(request: Request, response: Response): void {
   response.status(200).json({
-    message: "hello13",
+    message: "hello14",
   });
 }
 
